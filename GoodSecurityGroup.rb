ce782ea36076c1e72edfd182efe64f7c5716570a
citrusfizz@gmail.com <citrusfizz@gmail.com>
2015-10-20 13:06:14 -0700

    added some new scripts

---
+++ GoodSecurityGroup.rb
@@ -0,0 +1,40 @@
+require 'yaml'
+require 'right_api_client'
+require 'json'
+
+creds = YAML.load_file("#{ENV['HOME']}/.rest_connection/rest_api_config.yaml")
+
+# Extract the account
+creds[:account] = File.basename(creds[:api_url])
+
+# Figure out url to hit:
+creds[:api_url] = "https://"+URI.parse(creds[:api_url]).host
+
+@endpoint = URI.parse(creds[:api_url]).host
+
+@client = RightApi::Client.new(:email => creds[:user], :password => creds[:pass], :account_id => creds[:account], :api_url => creds[:api_url], :timeout => nil)
+
+
+allClouds = @client.clouds.index
+allClouds.each do |cloud|
+  cloud.security_groups.index.each do |secGroup|
+    secGroup.security_group_rules.index.each do |rule|
+      begin
+       if rule.show.direction === "ingress"
+         sgroup = secGroup.show.links[1]['href'].split("/")[5]
+         snetwork = secGroup.show.links[2]['href'].split("/")[3]
+         print "#{cloud.name},#{secGroup.name},#{snetwork},#{sgroup},#{rule.show.source_type},#{rule.show.cidr_ips},#{rule.show.protocol},"
+		 if rule.show.protocol != "all"
+           puts "#{rule.show.start_port},#{rule.show.end_port}"
+		 else
+		    puts "all,all"
+         end
+
+       end
+      rescue => e
+      next
+      end
+    end
+  end
+end
+
---
+++ InstanceQ.rb
@@ -0,0 +1,80 @@
+require 'yaml'
+require 'right_api_client'
+require 'json'
+
+creds = YAML.load_file("#{ENV['HOME']}/.rest_connection/rest_api_config.yaml")
+
+# Extract the account
+creds[:account] = File.basename(creds[:api_url])
+
+# Figure out url to hit:
+creds[:api_url] = "https://"+URI.parse(creds[:api_url]).host
+
+@endpoint = URI.parse(creds[:api_url]).host
+
+
+accountn = ARGV[0]
+credsapi = creds[:api_url]
+email = creds[:user]
+pass = creds[:pass]
+
+@endpoint = URI.parse(credsapi).host
+
+@client = RightApi::Client.new(:email => email, :password => pass,
+                              :account_id => accountn, :api_url => credsapi,
+                              :timeout => nil)
+
+def api16_call(query)
+
+  get  = Net::HTTP::Get.new(query)
+  get['Cookie']        = @client.cookies.map { |key, value| "%s=%s" % [key, value] }.join(';')
+  get['X-Api_Version'] = '1.6'
+  get['X-Account']     = @client.account_id
+  http = Net::HTTP.new(@endpoint, 443)
+  http.use_ssl = true
+
+  response = http.request(get)
+  return response
+
+  end
+
+
+def all_instances()
+
+  filters_list = "state=operational"
+  filters = CGI::escape(filters_list)
+
+  query="/api/instances?view=full&filter="+filters
+  response = api16_call(query)
+
+  body_data = response.body
+
+  result = JSON.parse(body_data)
+
+  return result
+end
+
+
+instances = all_instances
+
+instances.each {|i|
+
+#trim the last bit of the incarnator
+#hack alert
+i['security_groups'].each do |dd|
+
+#puts i['security_groups']['name']
+endlink = i['links']['incarnator']['href']
+endlink.slice! "/api"
+cloud = i['links']['cloud']['name']
+network = i['networks'].first['href'].split("/").last
+secgroup = dd['href'].split("/").last
+sg = i['links']
+#Build the url
+#https://my.rightscale.com/acct/9202/servers/1201734003
+url = credsapi+"/acct/"+accountn+endlink
+
+puts "#{cloud},#{i['name']},#{url},#{dd['name']},#{network},#{secgroup}"
+end
+}
+
